f04ed62cd1ee23811e4a29a905380af320131d9c
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-02-20 23:27:06 +0100

    Changed the name of the welcome.blade file

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('guest.welcome');
-})->name('guest.welcome');
+    return view('guest.home');
+})->name('guest.home');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
