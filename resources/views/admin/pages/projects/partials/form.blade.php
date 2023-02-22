3f99569cb1d3f8f567e2e5296568672c66b15480
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2023-02-22 12:37:58 +0100

    Updated the form partial file by adding in it an header

--- resources/views/admin/pages/projects/partials/form.blade.php
+++ resources/views/admin/pages/projects/partials/form.blade.php
@@ -8,20 +8,27 @@
   @csrf
   @method($formMethod)
 
-  <div class="mb-3">
-    <label for="title" class="form-label">Title</label>
-    <input class="my_form-el form-control @error('title') is-invalid @enderror" id="title" name="title" aria-describedby="title-errors" placeholder="Insert the title" minlength="2" maxlength="255" value="{{old('title',$project->title)}}">
-    @error('title')
-    <div class="form-text invalid-feedback">{{$message}}</div>
-    @enderror
-  </div>
-
-  <div class="mb-3">
-    <label for="description" class="form-label">Description</label>
-    <textarea class="my_form-el form-control  @error('descripttion') is-invalid @enderror"" id="description" cols="30" rows="10" name="description" aria-describedby="description-errors" minlength="10" placeholder="Insert the description">{{old('description',$project->description)}}</textarea>
-    @error('description')
-    <div class="form-text invalid-feedback">{{$message}}</div>
-    @enderror
-  </div>
-  <button type="submit" class="btn btn-primary my_btn">Submit</button>
+  <article class="card">
+    <div class="card-header">
+      <h2 class="text-center m-0 p-3 fw-bold">{{$formMethod === 'POST' ? 'Create a new project' : "Edit the project '$project->title'"}}</h2>
+    </div>
+    <div class="card-body">
+      <div class="form-outline mb-3">
+        <label for="title" class="form-label">Title</label>
+        <input class="my_form-el form-control @error('title') is-invalid @enderror" id="title" name="title" aria-describedby="title-errors" placeholder="Insert the title" minlength="2" maxlength="255" value="{{old('title',$project->title)}}">
+        @error('title')
+        <div class="form-text invalid-feedback">{{$message}}</div>
+        @enderror
+      </div>
+    
+      <div class="form-outline mb-3">
+        <label for="description" class="form-label">Description</label>
+        <textarea class="my_form-el form-control  @error('descripttion') is-invalid @enderror"" id="description" cols="30" rows="10" name="description" aria-describedby="description-errors" minlength="10" placeholder="Insert the description">{{old('description',$project->description)}}</textarea>
+        @error('description')
+        <div class="form-text invalid-feedback">{{$message}}</div>
+        @enderror
+      </div>
+      <button type="submit" class="btn btn-primary my_btn">Submit</button>
+    </div>
+  </article>
 </form>
